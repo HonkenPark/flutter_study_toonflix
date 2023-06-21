71963cba2b501613080ea267e37d4d50346ab74d
박홍근 <sizhawk@mediazen.co.kr>
2023-06-21 19:02:03 +0900

    FEAT(main): Button and Container
    21th June Task

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,8 @@ class App extends StatelessWidget {
         backgroundColor: Color(0xFF181818),
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Column(children: [
+          child:
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
             SizedBox(
               height: 50,
             ),
@@ -53,6 +54,51 @@ class App extends StatelessWidget {
                 ),
               ],
             ),
+            SizedBox(
+              height: 120,
+            ),
+            Text(
+              'Total Balance',
+              style: TextStyle(
+                fontSize: 22,
+                color: Colors.white.withOpacity(0.8),
+              ),
+            ),
+            SizedBox(
+              height: 5,
+            ),
+            Text(
+              '\$5 194 482',
+              style: TextStyle(
+                fontSize: 48,
+                color: Colors.white,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    shape: BoxShape.circle,
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: 20,
+                      horizontal: 50,
+                    ),
+                    child: Text('Transfer',
+                      style: TextStyle(
+                        fontSize: 22,
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            )
           ]),
         ),
       ),
