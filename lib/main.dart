8cffb9a3157efb8656d16c6a9691bd799788a767
박홍근 <sizhawk@mediazen.co.kr>
2023-06-30 18:27:33 +0900

    FEAT(webtoon): Image, Navitator, ListView
    Create ListView, Route, fix CORS error

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'dart:io';
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:toonflix/screens/home_screen.dart';
 
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
+}
+
 void main() {
+  // HttpOverrides.global = MyHttpOverrides();
   runApp(const App());
 }
 
@@ -10,8 +23,17 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: HomeScreen(),
+      scrollBehavior: const MaterialScrollBehavior().copyWith(
+        dragDevices: {
+          PointerDeviceKind.mouse,
+          PointerDeviceKind.touch,
+          PointerDeviceKind.stylus,
+          PointerDeviceKind.trackpad,
+          PointerDeviceKind.unknown,
+        },
+      ),
     );
   }
 }
